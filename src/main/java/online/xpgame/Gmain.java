9d67f4bc10f988656e1a3fc55384008cb62eb845
xiaopeng <18971903450@163.com>
2017-05-08 14:59:43 +0800

    Initial commit

---
+++ src/main/java/online/xpgame/Gmain.java
@@ -0,0 +1,22 @@
+package online.xpgame;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by xiaopeng on 2017/5/8.
+ */
+public class Gmain {
+    private static final Logger logger = LoggerFactory.getLogger(Gmain.class);
+    public static void main(String[] args) {
+
+        System.out.println("hello word !");
+        logger.info("hello word !");
+        try {
+            Thread.sleep(90000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        logger.info("退出");
+    }
+}
